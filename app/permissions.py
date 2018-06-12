b99f2d2b5bb861a6a9145d0d36c454e8066381fc
YihongH <shirleyhuang@gwu.edu>
2018-06-12 13:49:09 -0400

    revise OrderPermission

--- app/permissions.py
+++ app/permissions.py
@@ -5,28 +5,32 @@ class OrderPermissions(permissions.BasePermission):
     """
     Custom permission to only allow owners of an object to edit it.
     """
-
     def has_permission(self, request, view):
-        if request.method in ['GET']:
-            return request.user.has_perm('app.view_order')        
-        if request.method in ['POST']:
-            return request.user.has_perm('app.add_order')
-        if request.method in ['PUT', 'PATCH']:
-            return request.user.has_perm('app.change_order')
-        if request.method in ['DELETE']:
-            return request.user.has_perm('app.delete_order')
-        return False
+        options = {'GET': request.user.has_perm('app.view_order'), 
+           'POST': request.user.has_perm('app.add_order'), 
+           'PUT': request.user.has_perm('app.change_order'), 
+           'PATCH': request.user.has_perm('app.change_order'), 
+           'DELETE': request.user.has_perm('app.delete_order')}
+        return options.get(request.method, False)
 
-    def has_object_permission(self, request, view, obj):         
-        if request.method in ['GET']:
-            return request.user.has_perm('app.view_order', obj)        
-        if request.method in ['POST']:
-            return request.user.has_perm('app.add_order', obj)
-        if request.method in ['PUT', 'PATCH']:
-            return request.user.has_perm('app.change_order', obj)
-        if request.method in ['DELETE']:
-            return request.user.has_perm('app.delete_order', obj)
-        return False
+    # def has_permission(self, request, view):
+    #     if request.method in ['GET']:
+    #         return request.user.has_perm('app.view_order')        
+    #     if request.method in ['POST']:
+    #         return request.user.has_perm('app.add_order')
+    #     if request.method in ['PUT', 'PATCH']:
+    #         return request.user.has_perm('app.change_order')
+    #     if request.method in ['DELETE']:
+    #         return request.user.has_perm('app.delete_order')
+    #     return False
+
+    def has_object_permission(self, request, view, obj):
+        options_for_object = {'GET': request.user.has_perm('app.view_order', obj), 
+           'POST': request.user.has_perm('app.add_order', obj), 
+           'PUT': request.user.has_perm('app.change_order', obj), 
+           'PATCH': request.user.has_perm('app.change_order', obj), 
+           'DELETE': request.user.has_perm('app.delete_order', obj)}
+        return options_for_object.get(request.method, False)
 
 class CustomObjectPermissions(permissions.DjangoObjectPermissions):
     """
